53b2718378d12695a73c358c972c70b5209ad06d
a-voinov <alariht@gmail.com>
2019-01-22 20:46:45 +0700

    GET Customers

---
+++ src/main/java/com/tutorial/springdemo/dao/CustomerDAO.java
@@ -0,0 +1,17 @@
+package com.tutorial.springdemo.dao;
+
+import java.util.List;
+
+import com.tutorial.springdemo.entity.Customer;
+
+public interface CustomerDAO {
+
+	public List<Customer> getCustomers();
+
+	public void saveCustomer(Customer theCustomer);
+
+	public Customer getCustomer(int theId);
+
+	public void deleteCustomer(int theId);
+	
+}
