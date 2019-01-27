13b80f50b5124064b5eb06f3a2399069c0684b33
a-voinov <alariht@gmail.com>
2019-01-27 12:21:38 +0700

    Securing REST Endpoints with Spring Security

---
+++ src/main/java/com/tutorial/springdemo/config/SecurityWebApplicationInitializer.java
@@ -0,0 +1,8 @@
+package com.tutorial.springdemo.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+//Enable Spring Security filters for web application
+public class SecurityWebApplicationInitializer
+        extends AbstractSecurityWebApplicationInitializer {
+}
